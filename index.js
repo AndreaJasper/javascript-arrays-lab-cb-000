af7e406f39761ab1d8d0397b12be78166d3bf44b
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-21 17:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyAppendKitten(name){
   kittens.push(name);
 }
 
-function destructivelyPrependKitten() {
+function destructivelyPrependKitten(name) {
   kittens.unshift(name);
 }
 
